6f96d9183c39dc909bb0e94526dad358246c9a88
vludlsswithdevelop <vladislav.mikkoev@yandex.ru>
2024-01-08 06:04:15 +0300

    feat: Добавить авторизацию в вк клиенте

---
+++ VkStatusChanger.Worker/Models/InputArgs.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace VkStatusChanger.Worker.Models
+{
+    public class InputArgs
+    {
+        [Option("application-id", Required = true)]
+        public ulong ApplicationId { get; set; }
+        [Option("login", Required = true)]
+        public required string Login { get; set; }
+        [Option("password", Required = true)]
+        public required string Password { get; set; }
+        [Option("two-factor-auth", Required = true)]
+        public bool TwoFactorAuth { get; set; }
+    }
+}
--- VkStatusChanger.Worker/Program.cs
+++ VkStatusChanger.Worker/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace VkStatusChanger
+﻿using CommandLine;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkStatusChanger.Worker.Models;
+
+namespace VkStatusChanger
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            await Parser.Default.ParseArguments<InputArgs>(args)
+                .WithParsedAsync(async inputArgs =>
+                {
+                    var builder = Host.CreateApplicationBuilder(args);
+
+                    builder.Services.AddScoped<IVkApi>(provider =>
+                    {
+                        var vkApi = new VkApi();
+
+                        var authParams = new ApiAuthParams
+                        {
+                            ApplicationId = inputArgs.ApplicationId,
+                            Login = inputArgs.Login,
+                            Password = inputArgs.Password,
+                            Settings = Settings.Status,
+                        };
+
+                        if (inputArgs.TwoFactorAuth)
+                        {
+                            authParams.TwoFactorAuthorization = () =>
+                            {
+                                Console.WriteLine("Введите код двух факторной авторизации");
+                                return Console.ReadLine()!.Trim();
+                            };
+                        }
+
+                        vkApi.Authorize(authParams);
+
+                        return vkApi;
+                    });
+
+                    var host = builder.Build();
+                    await host.RunAsync();
+                });
         }
     }
 }

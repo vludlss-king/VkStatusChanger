3890b44c2b545774c6cbf40485f2204d15dc7b7b
vludlss <vladislav.mikkoev@yandex.ru>
2024-01-13 05:55:35 +0300

    refactor: Отделить подготовку теста в отдельный метод

--- VkStatusChanger.Worker.Tests/IntegrationTests/ConfigCommandControllerTests.cs
+++ VkStatusChanger.Worker.Tests/IntegrationTests/ConfigCommandControllerTests.cs
@@ -8,36 +8,41 @@ namespace VkStatusChanger.Worker.Tests.IntegrationTests
 {
     public class ConfigCommandControllerTests
     {
-        private readonly ISettingsHelper _settingsHelper;
-        private readonly ConfigCommandController _sut;
-
         public ConfigCommandControllerTests()
         {
-            var settingsFileStub = new Mock<IOptions<SettingsFile>>();
-            settingsFileStub
-                .Setup(setup => setup.Value)
-                .Returns(new SettingsFile { Name = "settings_test.json" });
-
-            _settingsHelper = new SettingsHelper(settingsFileStub.Object);
-
-            _sut = new ConfigCommandController(_settingsHelper);
+            
         }
 
         [Fact]
         public async Task Config_every_set_command_works_properly()
         {
+            var (settingsHelper, sut) = Startup("settings_config_every_set.json");
             SettingsCommand.EveryCommand.SetCommand command = new SettingsCommand.EveryCommand.SetCommand
             {
                 StatusesTexts = new List<string> { "Status1", "Status2", "Status3" },
                 Seconds = 60,
             };
 
-            await _sut.EverySet(command);
+            await sut.EverySet(command);
 
-            var settings = await _settingsHelper.ReadSettings();
+            var settings = await settingsHelper.ReadSettings();
 
             settings.Every.StatusesTexts.Should().BeEquivalentTo(command.StatusesTexts);
             settings.Every.Seconds.Should().Be(command.Seconds);
+            settings.Schedule.Items.Should().BeEmpty();
+        }
+
+        private (ISettingsHelper settingsHelper, ConfigCommandController sut) Startup(string fileName)
+        {
+            var settingsFileStub = new Mock<IOptions<SettingsFile>>();
+            settingsFileStub
+                .Setup(setup => setup.Value)
+                .Returns(new SettingsFile { Name = fileName });
+
+            var settingsHelper = new SettingsHelper(settingsFileStub.Object);
+            var sut = new ConfigCommandController(settingsHelper);
+
+            return (settingsHelper, sut);
         }
     }
 }

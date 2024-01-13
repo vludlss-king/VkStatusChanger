1ce0f48d7005beba3e46dd550640c3843e89b9ef
vludlss <vladislav.mikkoev@yandex.ru>
2024-01-13 06:08:20 +0300

    test: Добавить тест на команду
    settings schedule add

--- VkStatusChanger.Worker.Tests/IntegrationTests/ConfigCommandControllerTests.cs
+++ VkStatusChanger.Worker.Tests/IntegrationTests/ConfigCommandControllerTests.cs
@@ -14,9 +14,9 @@ namespace VkStatusChanger.Worker.Tests.IntegrationTests
         }
 
         [Fact]
-        public async Task Config_every_set_command_works_properly()
+        public async Task Settings_every_set_command_works_properly()
         {
-            const string fileName = "settings_config_every_set.json";
+            const string fileName = "settings_every_set.json";
             var (settingsHelper, sut) = Startup(fileName);
             SettingsCommand.EveryCommand.SetCommand command = new SettingsCommand.EveryCommand.SetCommand
             {
@@ -33,6 +33,32 @@ namespace VkStatusChanger.Worker.Tests.IntegrationTests
             settings.Every!.StatusesTexts.Should().BeEquivalentTo(command.StatusesTexts);
             settings.Every.Seconds.Should().Be(command.Seconds);
             settings.Schedule!.Items.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Settings_schedule_add_command_works_property()
+        {
+            const string fileName = "settings_schedule_add.json";
+            var (settingsHelper, sut) = Startup(fileName);
+            SettingsCommand.ScheduleCommand.AddCommand command = new SettingsCommand.ScheduleCommand.AddCommand
+            {
+                StatusText = "Status1",
+                Date = new DateTime(2024, 1, 13),
+                Time = new TimeSpan(6, 5, 30),
+            };
+
+            await sut.ScheduleAdd(command);
+
+            var settings = await settingsHelper.ReadSettings();
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+
+            settings.Schedule.Items.Count.Should().Be(1);
+            settings.Schedule.Items.First().StatusText.Should().Be(command.StatusText);
+            settings.Schedule.Items.First().Date.Should().Be(command.Date);
+            settings.Schedule.Items.First().Time.Should().Be(command.Time);
+            settings.Every.StatusesTexts.Count.Should().Be(0);
+            settings.Every.Seconds.Should().Be(0);
         }
 
         private (ISettingsHelper settingsHelper, ConfigCommandController sut) Startup(string fileName)

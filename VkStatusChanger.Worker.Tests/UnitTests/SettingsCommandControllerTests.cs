b723245a43da42160d918061a69b9bda25d68748
vludlss <vladislav.mikkoev@yandex.ru>
2024-01-14 09:01:52 +0300

    test: Добавить unit-test
    На проверку отображения вывода в консоль для команды `settings type show`

--- VkStatusChanger.Worker.Tests/UnitTests/SettingsCommandControllerTests.cs
+++ VkStatusChanger.Worker.Tests/UnitTests/SettingsCommandControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using VkStatusChanger.Worker.Contracts.Helpers;
+﻿using VkNet.Enums.Filters;
+using VkStatusChanger.Worker.Contracts.Helpers;
 using VkStatusChanger.Worker.Contracts.Infrastructure;
 using VkStatusChanger.Worker.Controllers;
 using VkStatusChanger.Worker.Enums;
@@ -10,7 +11,7 @@ namespace VkStatusChanger.Worker.Tests.UnitTests
     public class SettingsCommandControllerTests
     {
         [Fact]
-        public async Task Settings_auth_set_command_shows_expected_output()
+        public async Task Settings_type_set_command_shows_expected_output()
         {
             var parserResultStub = new Mock<ICustomParserResult>();
             var settingsHelperStub = new Mock<ISettingsHelper>();
@@ -29,6 +30,25 @@ namespace VkStatusChanger.Worker.Tests.UnitTests
             var output = await sut.TypeSet(command);
 
             output.Should().Be("Тип настроек изменён.");
+        }
+
+        [Fact]
+        public async Task Settings_type_show_command_shows_expected_output()
+        {
+            var parserResultStub = new Mock<ICustomParserResult>();
+            var settingsHelperStub = new Mock<ISettingsHelper>();
+            settingsHelperStub
+                .Setup(setup => setup.ReadSettings())
+                .Returns(Task.FromResult(new SettingsModel()
+                {
+                    SettingsType = SettingsType.Schedule
+                }));
+            var sut = new SettingsCommandController(parserResultStub.Object, settingsHelperStub.Object);
+            SettingsCommand.TypeCommand.ShowCommand command = new SettingsCommand.TypeCommand.ShowCommand();
+
+            var output = await sut.TypeShow(command);
+
+            output.Should().Be($"Тип настроек: {SettingsType.Schedule}");
         }
     }
 }

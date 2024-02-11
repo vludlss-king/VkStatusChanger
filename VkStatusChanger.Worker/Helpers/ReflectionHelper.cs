cd8dbacecd40818b30203a1896064cb8b3d0a94f
vludlss <vladislav.mikkoev@yandex.ru>
2024-02-11 05:16:50 +0300

    feat: Добавить метод установки значения приватному свойству

---
+++ VkStatusChanger.Worker/Helpers/ReflectionHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace VkStatusChanger.Worker.Helpers;
+
+internal static class ReflectionHelper
+{
+    public static void SetPrivatePropertyValue<TValue>(object obj, string propertyName, TValue value)
+    {
+        Type type = obj.GetType();
+        var property = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        if (property == null)
+            throw new ArgumentOutOfRangeException(nameof(propertyName), $"Свойство {propertyName} не было найдено в типе {type.FullName}");
+        property.SetMethod.Invoke(obj, new object[] { value! });
+    }
+}
